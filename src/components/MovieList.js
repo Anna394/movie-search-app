dc1b0eea12336c0349e1d5737996990a964c0b65
Anna Petryaeva <flo.and.mik@yandex.ru>
2025-02-18 15:46:18 +0300

    123

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { Card } from 'antd';
+import { format } from 'date-fns';
 
 import { getMovies } from '../api';
 import './MovieList.css';
 
+const { Meta } = Card;
+
+// Функция для усечения текста без обрезки слов
 function truncateText(text, maxLength) {
   if (text.length <= maxLength) return text;
 
@@ -17,17 +21,22 @@ function truncateText(text, maxLength) {
   return truncated + '...';
 }
 
+function formatDate(dateString) {
+  if (!dateString) return 'Unknown date'; // Обработка пустых значений
+  return format(new Date(dateString), 'MMMM d, yyyy'); // Пример: March 5, 2020
+}
+
 class MovieList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movies: [], // Изначально массив фильмов пустой
+      movies: [],
     };
   }
 
   componentDidMount() {
     getMovies().then((movies) => {
-      this.setState({ movies }); // Обновляем состояние после загрузки данных
+      this.setState({ movies });
     });
   }
 
@@ -35,15 +44,27 @@ class MovieList extends Component {
     return (
       <div className="movie-list">
         {this.state.movies.map((movie) => (
-          <Card key={movie.id} className="movie-card">
-            <div className="movie-content">
-              <img alt={movie.title} src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-              <div className="movie-info">
-                <h3>{movie.title}</h3>
-                <p className="release-date">{movie.release_date}</p>
-                <p className="description">{truncateText(movie.overview, 200)}</p>
-              </div>
-            </div>
+          <Card
+            key={movie.id}
+            hoverable
+            className="movie-card"
+            cover={
+              <img
+                alt={movie.title}
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                style={{ width: 180, height: 280, objectFit: 'cover' }}
+              />
+            }
+          >
+            <Meta
+              title={movie.title}
+              description={
+                <>
+                  <p className="release-date">{formatDate(movie.release_date)}</p>
+                  <p className="description">{truncateText(movie.overview, 200)}</p>
+                </>
+              }
+            />
           </Card>
         ))}
       </div>
